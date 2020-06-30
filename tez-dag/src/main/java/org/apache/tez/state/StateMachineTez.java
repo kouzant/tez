005648dbb1d3a4cc22e3664973e9e0938678dc96
Fabio Buso <buso.fabio@gmail.com>
2020-06-30 10:30:52 +0200

    Bump hadoop dependency to 3.2.0.0-SNAPSHOT

--- tez-dag/src/main/java/org/apache/tez/state/StateMachineTez.java
+++ tez-dag/src/main/java/org/apache/tez/state/StateMachineTez.java
@@ -51,9 +51,6 @@ public class StateMachineTez<STATE extends Enum<STATE>, EVENTTYPE extends Enum<E
     return realStatemachine.getCurrentState();
   }
 
-  @Override
-  public void setCurrentState(STATE currentState) { realStatemachine.setCurrentState(currentState); }
-
   @SuppressWarnings("unchecked")
   @Override
   public STATE doTransition(EVENTTYPE eventType, EVENT event) throws
